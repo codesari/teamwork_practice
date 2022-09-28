3587d7352879f5e2a15b27e46622c6fc6e431c5c
codesari <ahmtsr81@gmail.com>
2022-09-28 20:28:55 +0300

    file arrangement

---
+++ weather_solution/weather.js
@@ -0,0 +1,14 @@
+const form = document.querySelector("section.top-banner form");
+const input = document.querySelector(".container input");
+const msg = document.querySelector(".span msg");
+const list = document.querySelector(".ajax-section .cities");
+
+// ! token
+localStorage.setItem(
+  "tokenKey",
+  "5EtZlhgMu9CQn7zV4uD4F5zFYGqxKzmN9vobbH6/7alkWz+fJD7WKuejncIrqdsH"
+);
+// localStorage.setItem(
+//   "encryptedTokenKey",
+//   EncryptStringAES("c596300e44a2dbdfa3b87cda29ff8d7b")
+// );
